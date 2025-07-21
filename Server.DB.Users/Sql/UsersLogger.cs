616f109d881770a7624a49de03ed565827146dce
Autoritet174 <Autoritet174@yandex.ru>
2025-07-21 22:51:34 +0800

    1

--- Server.DB.Users/Sql/UsersLogger.cs
+++ Server.DB.Users/Sql/UsersLogger.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Npgsql;
 using NpgsqlTypes;
+using Server.DB.Users.Migrations;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.Json.Nodes;
 namespace Server.DB.Users.Sql;
 public static class UsersLogger
 {
+    /// <summary>
+    /// Записывает данные о входе пользователя и хеш оборудования в таблицу users_authorization_logs.
+    /// </summary>
+    /// <param name="obj">JSON-объект с информацией об устройстве.</param>
+    /// <param name="user_id">Идентификатор пользователя.</param>
+    /// <param name="email">Электронная почта пользователя.</param>
+    /// <param name="password_hash">Хеш пароля пользователя.</param>
+    /// <param name="ip_address">IP-адрес клиента.</param>
+    /// <exception cref="ArgumentNullException">Если obj или email null.</exception>
     public static async Task WriteLog(JsonObject obj, Guid user_id, string email, string password_hash, NpgsqlInet ip_address)
     {
 
@@ -17,6 +29,24 @@ public static class UsersLogger
         string? graphicsDeviceName = JsonObjectHelper.GetStringN(obj, "graphicsDeviceName", 255);
         int? graphicsMemorySize = JsonObjectHelper.GetIntegerN(obj, "graphicsMemorySize");
         string? deviceUniqueIdentifier = JsonObjectHelper.GetStringN(obj, "deviceUniqueIdentifier", 255);
+
+
+        string split = "___";
+        StringBuilder stringBuilder = new();
+        stringBuilder.Append(deviceModel ?? string.Empty); stringBuilder.Append(split);
+        stringBuilder.Append(deviceType ?? string.Empty); stringBuilder.Append(split);
+        stringBuilder.Append(operatingSystem ?? string.Empty); stringBuilder.Append(split);
+        stringBuilder.Append(processorType ?? string.Empty); stringBuilder.Append(split);
+        stringBuilder.Append(processorCount ?? 0); stringBuilder.Append(split);
+        stringBuilder.Append(systemMemorySize ?? 0); stringBuilder.Append(split);
+        stringBuilder.Append(graphicsDeviceName ?? string.Empty); stringBuilder.Append(split);
+        stringBuilder.Append(graphicsMemorySize ?? 0); stringBuilder.Append(split);
+        stringBuilder.Append(deviceUniqueIdentifier ?? string.Empty);
+
+
+        byte[] bytes = Encoding.UTF8.GetBytes(stringBuilder.ToString());
+        byte[] hashBytes = SHA256.HashData(bytes);
+
 
         Db db = new();
         string sql = $"""
@@ -36,7 +66,8 @@ public static class UsersLogger
                 system_memory_size,
                 graphics_device_name,
                 graphics_memory_size,
-                device_unique_identifier
+                device_unique_identifier,
+                hash_sha256
             ) VALUES (
                 @id,
                 @user_id,
@@ -53,7 +84,8 @@ public static class UsersLogger
                 @systemMemorySize,
                 @graphicsDeviceName,
                 @graphicsMemorySize,
-                @deviceUniqueIdentifier
+                @deviceUniqueIdentifier,
+                @hash_sha256
             );
             """;
 
@@ -72,7 +104,8 @@ public static class UsersLogger
             new NpgsqlParameter("systemMemorySize", systemMemorySize),
             new NpgsqlParameter("graphicsDeviceName", graphicsDeviceName),
             new NpgsqlParameter("graphicsMemorySize", graphicsMemorySize),
-            new NpgsqlParameter("deviceUniqueIdentifier", deviceUniqueIdentifier)
+            new NpgsqlParameter("deviceUniqueIdentifier", deviceUniqueIdentifier),
+            new NpgsqlParameter("hash_sha256", hashBytes)
         );
 
     }
